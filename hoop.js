bb9f0e9e3f769fcafb1043fc02d012ee1eb96866
tinywomanintech <58062886+tinywomanintech@users.noreply.github.com>
2021-04-20 20:56:37 +0700

    Create hoop.js
    @litanur , done mba lita

---
+++ hoop.js
@@ -0,0 +1,10 @@
+function hoopCount (n) {
+   //your code goes here   
+  for (let i = 0; i < n; i++) {
+    if (n >= 10){
+      return "Great, now move on to tricks"
+    } else {
+      return "Keep at it until you get it"
+    }
+  }
+}
